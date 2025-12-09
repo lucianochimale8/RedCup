881147fcda46a3fe62e3c3920e9f7b1fd15168af
lucianochimale8 <kchimale@gmail.com>
2025-12-09 16:33:47 -0300

    metodo para el flip del personaje

--- RedCup/Assets/Scripts/Player/PlayerMovement.cs
+++ RedCup/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rb;
 
+    private bool isFacingRight = true;
 
     private void Awake()
     {
@@ -14,5 +15,20 @@ public class PlayerMovement : MonoBehaviour
     public void Move(Vector2 input) 
     {
         rb.linearVelocity = input * moveSpeed;
+        Flip(input);
+    }
+    private void Flip(Vector2 input)
+    {
+        if (input.x != 0)
+        {
+            if ((isFacingRight && input.x < 0f) || (!isFacingRight && input.x > 0f))
+            {
+                Vector3 scale = transform.localScale; // Variable referenciad de la escala
+                scale.x *= -1f; // invertir escala
+                transform.localScale = scale; // establecer nueva escala
+                isFacingRight = !isFacingRight; // invertir si se ha dado vuelta
+            }
+        }
+
     }
 }

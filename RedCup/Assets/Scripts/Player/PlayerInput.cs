bff210ceb82814a69ca97e4e7587f5a98b4ad6e3
lucianochimale8 <kchimale@gmail.com>
2025-12-10 12:21:42 -0300

    agregando al jugador las estrategias

--- RedCup/Assets/Scripts/Player/PlayerInput.cs
+++ RedCup/Assets/Scripts/Player/PlayerInput.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     public Vector2 MoveInput { get; private set; }
+    // Para reconocer cuando se esta corriendo
+    public bool IsRunning { get; private set; }
     private void Update()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
-
+        // solo este script puede escribir el movimiento
         MoveInput = new Vector2(x, y).normalized;
+        // Shift para correr
+        IsRunning = Input.GetKey(KeyCode.LeftShift);
     }
 }

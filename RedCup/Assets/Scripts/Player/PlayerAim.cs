c1c23a7898c7402a38c84e1bff3817471bdb5b84
lucianochimale8 <kchimale@gmail.com>
2025-12-10 19:37:28 -0300

    el arma sigue el puntero y gira si esta para la izquierda

---
+++ RedCup/Assets/Scripts/Player/PlayerAim.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float aimSpeed = 10f;
+
+    private Camera cam;
+    private Vector2 mousePos;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    
+    private void Update()
+    {
+        // el aim sigue al player
+        transform.position = playerTransform.position;
+
+        // posicion del mouse en world space
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        // direccion hacia el mouse
+        Vector2 direction = mousePos - (Vector2)transform.position;
+
+        // rotación suave
+        transform.right = Vector2.MoveTowards(transform.right, direction, aimSpeed * Time.deltaTime);
+
+        // flip visual
+        if (transform.right.x < 0)
+            transform.localScale = new Vector3(1, -1, 1);
+        else
+            transform.localScale = new Vector3(1, 1, 1);
+    }
+}

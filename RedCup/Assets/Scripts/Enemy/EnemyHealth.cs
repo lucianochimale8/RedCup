96f752fcdec11b2257e86f740d5f9f6577e5c5f9
lucianochimale8 <kchimale@gmail.com>
2025-12-17 11:19:47 -0300

    fix: los enemigos pueden recibir mas de un proyectil

--- RedCup/Assets/Scripts/Enemy/EnemyHealth.cs
+++ RedCup/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -3,31 +3,27 @@ using System.Collections;
 
 public class EnemyHealth : MonoBehaviour
 {
+    [Header("Vida Enemigo")]
     [SerializeField] private int maxHealth = 3;
     [SerializeField] private float hurtDuration = 0.2f;
 
     private int health;
     private SpriteRenderer spriteRenderer;
-
-    private bool isHurt = false;
-
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         health = maxHealth;
     }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
         Projectile projectile = collision.gameObject.GetComponent<Projectile>();
-        if (projectile != null && !isHurt)
+        if (projectile != null)
         {
             TakeDamage();
             projectile.gameObject.SetActive(false);
         }
     }
-
     public void TakeDamage()
     {
         health--;
@@ -40,20 +36,8 @@ public class EnemyHealth : MonoBehaviour
         }
 
         StopAllCoroutines();
-        StartCoroutine(HurtRoutine());
-    }
-
-    private IEnumerator HurtRoutine()
-    {
-        isHurt = true;
-
         StartCoroutine(Blink(hurtDuration));
-
-        yield return new WaitForSeconds(hurtDuration);
-
-        isHurt = false;
     }
-
     private IEnumerator Blink(float duration)
     {
         spriteRenderer.color = Color.red;

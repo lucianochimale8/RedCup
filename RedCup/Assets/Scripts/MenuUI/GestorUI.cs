189cfc5b146a1e514c9a6de737db12bd859f9ba4
lucianochimale8 <kchimale@gmail.com>
2026-02-05 18:01:40 -0300

    empezar de nuevo el menu ui

--- RedCup/Assets/Scripts/MenuUI/GestorUI.cs
+++ RedCup/Assets/Scripts/MenuUI/GestorUI.cs
@@ -1,37 +1,58 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GestorUI : MonoBehaviour
 {
+    [System.Serializable]
+    public class PanelEntry
+    {
+        public PanelType type;
+        public UIPanel panel;
+    }
+
     [Header("Colocar paneles en orden")]
-    public UIPanel[] paneles;
+    [SerializeField] private List<PanelEntry> paneles;
+
     private UIPanel currentPanel;
+
+    private Dictionary<PanelType, UIPanel> panelDict;
+
+    private void Awake()
+    {
+        panelDict = new Dictionary<PanelType, UIPanel>();
+
+        foreach (var entry in paneles)
+        {
+            panelDict.Add(entry.type, entry.panel);
+        }
+    }
 
     public void Start()
     {
+        Time.timeScale = 1f;
         OcultarPaneles();
-        MostrarPaneles(0);
+        MostrarPaneles(PanelType.MenuInicio);
     }
 
-    public void MostrarPaneles(int index)
+    public void MostrarPaneles(PanelType type)
     {
-        if (index < 0 || index >= paneles.Length)
+        if (!panelDict.ContainsKey(type))
         {
-            Debug.Log("error: incorrect index");
+            Debug.LogError("Panel no registrado: " + type);
+            return;
         }
+
         if (currentPanel != null)
-        {
             currentPanel.Ocultar();
-        }
-        paneles[index].Mostrar();
-        currentPanel = paneles[index];
+
+        currentPanel = panelDict[type];
+        currentPanel.Mostrar();
     }
 
     public void OcultarPaneles()
     {
-        for (int i = 0; i < paneles.Length; i++)
-        {
-            paneles[i].Ocultar();
-        }
+        foreach (var panel in panelDict.Values)
+            panel.Ocultar();
     }
 
     public void Salir()

77d407a3a922801bf6a8202740a333a8af17193a
lucianochimale8 <kchimale@gmail.com>
2026-02-05 19:01:54 -0300

    agregando estructura uipanel, paneltype y gestorui

---
+++ RedCup/Assets/Scripts/Menus/MenuJuego/MenuInicio.cs
@@ -0,0 +1,26 @@
+
+using UnityEngine;
+
+public class MenuInicio : UIPanel
+{
+    private GestorUI gestorUI;
+
+    private void Awake()
+    {
+        gestorUI = FindFirstObjectByType<GestorUI>();
+    }
+
+    public override void Mostrar()
+    {
+        
+    }
+
+    public override void Ocultar()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
+}

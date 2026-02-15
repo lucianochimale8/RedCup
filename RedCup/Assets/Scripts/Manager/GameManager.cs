64ab8114c057f4275344aa61a25878662002da4c
lucianochimale8 <kchimale@gmail.com>
2026-02-15 16:14:48 -0300

    modificando gameobject

--- RedCup/Assets/Scripts/Manager/GameManager.cs
+++ RedCup/Assets/Scripts/Manager/GameManager.cs
@@ -5,30 +5,23 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+
     [Header("Vidas")]
     [SerializeField] private int startingLives = 3;
     [SerializeField] private TMP_Text livesText;
-    private int lives; 
+    
+    private int currentLives; 
+    public bool IsPlayerDead { get; private set; }
 
     [Header("Niveles")]
     [SerializeField] private string escenaFinal = "Win";
 
-    [Header("Enemigos restantes y Waves")]
-    private int enemiesLeft;
-    private bool allWavesSpawned;
-
-    [Header("Collect Key y Altar destruido")]
-    private bool altarDestroyed;
-    private bool keyCollected;
-
     [Header("UI Nivel")]
     [SerializeField] private GameObject levelCompleteText;
 
-    public int Lives => lives;
-    public bool AllEnemiesDead => altarDestroyed && keyCollected && enemiesLeft <= 0 && allWavesSpawned;
-    public bool IsPlayerDead { get; private set; }
-
-    public static GameManager Instance { get; private set; }
+    public int Lives => currentLives;
+    
 
     #region Unity Lifecycle
 
@@ -44,9 +37,6 @@ public class GameManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
-
-        lives = startingLives;
-
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
     private void OnDestroy()
@@ -54,31 +44,11 @@ public class GameManager : MonoBehaviour
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
-    private void OnEnable()
-    {
-        Debug.Log("GameManager OnEnable - suscribiendo eventos");
-
-        GameEvents.OnPlayerHit += OnPlayerHit;
-        GameEvents.OnPlayerDied += OnPlayerDied;
-        GameEvents.OnEnemyKilled += OnEnemyKilled;
-        GameEvents.OnKeyCollected += OnKeyColleted;
-
-    }
-
-    private void OnDisable()
-    {
-        GameEvents.OnPlayerHit -= OnPlayerHit;
-        GameEvents.OnPlayerDied -= OnPlayerDied;
-        GameEvents.OnEnemyKilled -= OnEnemyKilled;
-        GameEvents.OnKeyCollected -= OnKeyColleted;
-    }
-
     private void Start()
     {
-        ResetWaves();
+        currentLives = startingLives;
         UpdateLivesText();
     }
-
     #endregion
 
     #region Scene Loaded
@@ -96,153 +66,38 @@ public class GameManager : MonoBehaviour
             }
         }
 
-        ResetLevelState();
+        IsPlayerDead = false;
         UpdateLivesText();
     }
-
-    #endregion
-
-    #region Level State
-
-    public void TryExitLevel()
-    {
-        Debug.Log("Intentando salir del nivel...");
-
-        if (AllEnemiesDead)
-        {
-            Debug.Log("Nivel completado!");
-            LoadNextLevel();
-        }
-        else
-        {
-            Debug.Log("No podés salir todavía. faltan:" + enemiesLeft);
-        }
-    }
-
-    private void CheckIfLevelCanBeCompleted()
-    {
-        if (keyCollected && enemiesLeft <= 0 && allWavesSpawned)
-        {
-            if (levelCompleteText != null)
-                levelCompleteText.SetActive(true);
-        }
-    }
-
-    public void OnAltarDestroyed()
-    {
-        Debug.Log("Altar destruido! - enemigos cercanos eliminados");
-
-        StopEnemiesSpawn();
-
-        enemiesLeft = 0;
-
-        altarDestroyed = true;
-        allWavesSpawned = true;
-    }
-
-    private void OnKeyColleted()
-    {
-        Debug.Log("Llave recolecatada!");
-        keyCollected = true;
-        CheckIfLevelCanBeCompleted();
-    }
-
-    private void ResetLevelState()
-    {
-        Debug.Log("Se reseteo el level state");
-
-        altarDestroyed = false;
-        keyCollected = false;
-
-        if (levelCompleteText != null)
-            levelCompleteText.SetActive(false);
-    }
-
     #endregion
 
     #region Player Events
-    private void StopPlayer()
-    {
-        PlayerController player = FindFirstObjectByType<PlayerController>();
-        if (player != null)
-            player.StopPlayer();
-    }
     private void OnPlayerHit()
     {
         Debug.Log("EVENTO: Player Hit recibido");
 
-        lives--;
+        currentLives--;
         UpdateLivesText();
 
-        StopEnemiesMovement();
-        StopEnemiesSpawn();
-        StopPlayer();
-
-        StartCoroutine(WaitAndRestart(0.6f));
-    }
-
-    private void OnPlayerDied()
-    {
-
-        Debug.Log("EVENTO: Player Died recibido");
-
-        if (IsPlayerDead) return;
-
-        IsPlayerDead = true;
-
-        lives--;
-        UpdateLivesText();
-
-        StopEnemiesMovement();
-        StopEnemiesSpawn();
-        StopPlayer();
-
-        StartCoroutine(WaitAndRestart(0.8f));
+        if (currentLives > 0)
+        {
+            StartCoroutine(WaitAndRestart(0.6f));
+        }
+        else
+        {
+            StartCoroutine(GameOverRoutine());
+        }
     }
 
     #endregion
 
-    #region Enemy Events
-
-    private void OnEnemyKilled()
-    {
-        Debug.Log("EVENTO: Enemy Killed recibido");
-
-        enemiesLeft--;
-
-        CheckIfLevelCanBeCompleted();
-    }
-
-    public void IncreaseEnemiesLeft()
-    {
-        enemiesLeft++;
-    }
-
-    public void SetAllWavesSpawned()
-    {
-        allWavesSpawned = true;
-    }
-
-    #endregion
-
-    #region Waves & Restart
-
-    private void ResetWaves()
-    {
-        enemiesLeft = 0;
-        allWavesSpawned = false;
-        IsPlayerDead = false;
-    }
-
+    #region Restart
     private IEnumerator WaitAndRestart(float delay)
     {
         yield return new WaitForSeconds(delay);
 
-        ResetWaves();
-
-        if (lives > 0)
+        if (currentLives > 0)
         {
-            ResetWaves();
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
         else
@@ -254,35 +109,11 @@ public class GameManager : MonoBehaviour
     {
         yield return new WaitForSeconds(1f);
 
-        lives = startingLives;
+        currentLives = startingLives;
 
         IsPlayerDead = false;
 
-        ResetWaves();
-        ResetLevelState();
-
         SceneManager.LoadScene(0); // Menu principal
-    }
-
-    #endregion
-
-    #region Stop Enemies
-
-    private void StopEnemiesSpawn()
-    {
-        Spawner spawner = FindFirstObjectByType<Spawner>();
-        if (spawner != null)
-            spawner.StopAllCoroutines();
-    }
-
-    private void StopEnemiesMovement()
-    {
-        EnemyIA[] enemies = FindObjectsByType<EnemyIA>(FindObjectsSortMode.None);
-
-        foreach (EnemyIA enemy in enemies)
-        {
-            enemy.StopMovement();
-        }
     }
 
     #endregion
@@ -292,26 +123,7 @@ public class GameManager : MonoBehaviour
     private void UpdateLivesText()
     {
         if (livesText != null)
-            livesText.text = $"Lives: {lives}";
-    }
-
-    #endregion
-
-    #region Level Progression
-
-    private void LoadNextLevel()
-    {
-        int currentIndex = SceneManager.GetActiveScene().buildIndex;
-        int nextIndex = currentIndex + 1;
-
-        if (nextIndex < SceneManager.sceneCountInBuildSettings)
-        {
-            SceneManager.LoadScene(nextIndex);
-        }
-        else
-        {
-            SceneManager.LoadScene(escenaFinal);
-        }
+            livesText.text = $"Lives: {currentLives}";
     }
 
     #endregion
